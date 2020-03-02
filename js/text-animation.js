f211aef662bee433ced3cffdcd38bd6ef7bea126
Robin Dollé <robin.dolle@outlook.be>
2020-03-02 19:56:44 +0100

    💫 Replace homepage subtitle with typewriter

---
+++ js/text-animation.js
@@ -0,0 +1,52 @@
+// Text animation
+
+const typeWrite = document.querySelector(".typewrite");
+const toRotate = typeWrite.getAttribute("data-type");
+const period = 800;
+
+const TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = period;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    const i = this.loopNum % this.toRotate.length;
+    const fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = this.txt;
+
+    var that = this;
+    var delta = 150 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    if (toRotate) {
+      new TxtType(typeWrite, JSON.parse(toRotate), period);
+    }
+};
